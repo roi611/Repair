67a8a99787b7a3ea1f29d298e5eaf9624c74c66e
roi611 <yui9@msn.com>
2022-10-17 17:35:06 +0900

    プラグインを追加

---
+++ src/roi611/Repair/Main.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace roi611\Repair;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
+use pocketmine\event\Lisnener;
+use pocketmine\utils\Config;
+
+use pocketmine\item\Durable;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Utils;
+use onebone\economyapi\EconomyAPI;
+
+use form\ModalForm;
+
+class Main extends PluginBase {
+
+    public function onEnable():void{
+        
+        $this->EconomyAPI = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+            'money' => '15000'
+        ));
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+
+        if(!$sender instanceof Player){
+
+            $sender->sendMessage("ゲーム内で実行してください");
+            return true;
+
+        }
+
+        $pay = $this->config->get("money");
+        $have = EconomyAPI::getInstance()->myMoney($sender);
+
+        if ($have < $pay) {
+            $sender->sendMessage("§eお金が足りません");
+            return true;
+        }
+
+        $item = $sender->getInventory()->getItemInHand();
+
+        if(!$item instanceof Durable){
+            $sender->sendMessage("§6修理可能なアイテムではありません");
+            return true;
+        }
+
+        if($item->getDamage() < 0){
+            $sender->sendMessage("§6修理の必要がありません");
+            return true;
+        }
+                
+        $form = new ModalForm;
+        $form->setTitle("UIRepair");
+        $unit = EconomyAPI::getInstance()->getMonetaryUnit();
+        $form->setContent("§e{$pay} {$unit}§r で修理しますか？\nあなたの所持金: §e{$have} {$unit}§r");
+        $form->setButton1("はい");
+        $form->setButton2("いいえ");
+        $form->setCallable(function(Player $player,$data){
+
+            if($data === null){
+                return;
+            }
+
+            if($data){
+
+                $pay = $this->config->get("money");
+                $have = EconomyAPI::getInstance()->myMoney($player);
+
+                if ($have < $pay) {
+                    $player->sendMessage("§eお金が足りません");
+                }
+
+                $item = $player->getInventory()->getItemInHand();
+
+                if(!$item instanceof Durable){
+                    $player->sendMessage("§6修理可能なアイテムではありません");
+                }
+
+                if($item->getDamage() < 0){
+                    $player->sendMessage("§6修理の必要がありません");
+                }
+
+                EconomyAPI::getInstance()->reduceMoney($player, $pay);
+                $player->getInventory()->setItemInHand($player->getInventory()->getItemInHand()->setDamage(0));
+                $unit = EconomyAPI::getInstance()->getMonetaryUnit();
+                $player->sendMessage("§e{$pay} {$unit}§r を使用して修理しました");
+
+            } else {
+                $player->sendMessage("キャンセルしました");
+            }
+
+        });
+
+        $sender->sendForm($form);
+
+        return true;
+
+    }
+
+}
